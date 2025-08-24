877283c848672e9a81751f780d5ac7bcc84468f7
rohanmohapatra <31756343+rohanmohapatra@users.noreply.github.com>
2025-08-24 13:06:46 -0700

    ci: remove to backport v1

--- src/components/portfolio/constants.ts
+++
@@ -1,4 +0,0 @@
-export const imagePath = '/images';
-export const githubUsername = 'rohanmohapatra';
-export const githubToken =
-  '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';
